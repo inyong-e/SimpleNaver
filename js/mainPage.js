73534070600e788bb68dac219fe42e9f9e17ed83
inyong-e <jiy8319@gmail.com>
2020-08-10 01:24:07 +0900

    검색입력 창 페이지 가운데 배치

---
+++ js/mainPage.js
@@ -0,0 +1,7 @@
+// 메인 페이지 UI 조정
+
+const header = document.getElementById("header");
+const gnb = document.getElementById("gnb");
+
+document.body.appendChild(gnb);
+document.body.appendChild(header);
